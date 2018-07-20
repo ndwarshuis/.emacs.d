10223ffa8cfa1b8a6c54aaefad9249e69e7ebcbc
petrucci4prez <natedwarshuis@gmail.com>
2018-07-19 23:45:37 -0400

    banned some keybindings and begand dired config

--- init.el
+++ init.el
@@ -53,3 +53,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:foreground "#292b2e" :background "#bc6ec5" :height 1.0 :box nil)))))
+(put 'dired-find-alternate-file 'disabled nil)
