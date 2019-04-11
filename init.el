86b931bb50c28500e087d23b2d6ae01a4ec2f205
ndwarshuis <natedwarshuis@gmail.com>
2019-04-11 18:29:14 -0400

    removed unecessary things

--- init.el
+++ init.el
@@ -38,11 +38,3 @@
   "The absolute path the main EMACS configuration file.")
 
 (org-babel-load-file nd/conf-main)
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:foreground "#292b2e" :background "#bc6ec5" :height 1.0 :box nil)))))
-(put 'upcase-region 'disabled nil)
